5599f00ae8413125220408ae305bbd6e79f92f01
Brendan Kennedy <brendan.fj.kennedy@gmail.com>
2021-11-23 17:45:40 -0500

    working on creating new users

--- src/components/AccountForm.js
+++ src/components/AccountForm.js
@@ -8,28 +8,53 @@ import {
 
 function AccountForm() {
 
-    
-    
+    const [username, setUsername] = useState ('');
+    const [password, setPassword] = useState ('');   
+
+    const handleSubmit = (e) => {
+        e.preventDefault() 
+        const blog = {username,password}
+
+        console.log(blog)
+
+        fetch('"http://localhost:8080/make',{
+           method: 'POST' ,
+           headers: {"Content-Type": "application/json"}
+        })
+    }
     
     return (
      
-    <form>
+    <form onSubmit={handleSubmit}>
       <h1>
         Create a New Brendan's Blog Bazaar Account: 
       </h1> 
       <Box m = {1}>
-      <TextField id="standard-basic" label="Username" variant="standard" /> 
+      <TextField 
+      id="standard-basic" 
+      label="Username" 
+      variant="standard"
+      value = {username}
+      onChange = {(e) => setUsername(e.target.value)}
+    /> 
       </Box>
 
       <Box m = {1}>
-      <TextField id="standard-basic" label="Password" variant="standard" /> 
+      <TextField 
+      id="standard-basic" 
+      label="Password" 
+      variant="standard"
+      value = {password}
+      onChange = {(e) => setPassword(e.target.value)}
+    /> 
       </Box> 
 
       <Box m = {1}>
-      <Link to = "/login" style={{ textDecoration: 'none', color: 'white'}} >
-          <Button variant="contained" color="primary"> Create Account! </Button>
-      </Link>
+          <Button  type = 'submit' variant="contained" color="primary"> Create Account! </Button>
       </Box>
+
+      <p> Your Username that you created is: {username} </p>
+      <p> Your Password that you created is: {password} </p>
     </form> 
   
     );
